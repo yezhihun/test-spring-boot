d70a7ea3d95ff0d4661069e78b695f8b4e157435
tianye <yezhihun@163.com>
2019-05-20 15:51:11 +0800

    update

---
+++ readme.md
@@ -0,0 +1,3 @@
+spring-boot+rabbitmq+jpa+读写分离+  Redis+zk+dubbo
+
+高并发+多线程+sql优化
--- src/main/java/com/yezhihun/demo/MessageDTO.java
+++
@@ -1,9 +0,0 @@
-package com.yezhihun.demo;
-
-/**
- * Created by tianye on 2018/5/4.
- */
-public class MessageDTO {
-
-
-}
