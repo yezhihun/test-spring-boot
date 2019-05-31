08836fb3ed805ddd88647b0aae3108b65ab2303b
tianye <yezhihun@163.com>
2019-05-31 15:13:10 +0800

    update

--- src/main/java/com/yezhihun/demo/configuration/DemoApplication.java
+++ src/main/java/com/yezhihun/demo/configuration/DemoApplication.java
@@ -3,12 +3,13 @@ package com.yezhihun.demo.configuration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
 @ComponentScan(basePackages = "com.yezhihun.demo")
 @PropertySource(value = "config/app.properties")
-//@Import({DynamicDataSourceRegister.class})
+@Import({DynamicDataSourceRegister.class})
 public class DemoApplication {
 
 	public static void main(String[] args) {
