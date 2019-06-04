6edcb50eb94a00eb66cad6fe40e87c0f1011817d
tianye <yezhihun@163.com>
2019-06-04 17:44:12 +0800

    update

---
+++ src/test/java/com/yezhihun/demo/service/impl/CheckPointServiceImplTest.java
@@ -0,0 +1,23 @@
+package com.yezhihun.demo.service.impl;
+
+import com.yezhihun.demo.service.CheckPointService;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * Created by tianye on 2019/6/4.
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CheckPointServiceImplTest {
+    @Autowired
+    private CheckPointService checkPointService;
+    @Test
+    public void battleMap() throws Exception {
+
+    }
+
+}
